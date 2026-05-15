13fa3761bbd1fd458be4b1832e523c722c21d8a8
MerazMz <merazhaque74663@gmail.com>
2026-05-15 19:07:52 +0530

    fix homepage build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-import { getSettings } from "@/actions/settings";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const settings = await getSettings();
-
-  if (!settings) {
-    redirect("/onboarding");
-  }
-
   redirect("/dashboard");
 }
